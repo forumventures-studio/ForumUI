8d045806634e909275886b9de1c3a6c57013f54d
Waun Broderick <waunbroderick@gmail.com>
2024-05-21 15:42:01 -0400

    :memo: DOCS: Updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: "src/index.js", // Path to your library's entry point
+      name: "forumui",
+      formats: ["es", "umd"], // Common formats include UMD and ES
+      fileName: (format) => `forumui.${format}.js`,
+    },
+    rollupOptions: {
+      // Externalize peer dependencies to avoid bundling them into your package
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
